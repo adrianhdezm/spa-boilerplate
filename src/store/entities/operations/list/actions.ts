59b01e011aa6afba0ea66966a11263df3cee51a2
Adrian Hernandez-Mendez <aahdezm@gmail.com>
2019-06-06 02:38:54 +0200

    Add redux pagination without cache

--- src/store/entities/operations/list/actions.ts
+++ src/store/entities/operations/list/actions.ts
@@ -2,13 +2,20 @@ import { IEntity } from '@app/store/entities/models';
 import {
   LIST_ENTITIES_FAILURE,
   LIST_ENTITIES_START,
-  LIST_ENTITIES_SUCCESS
+  LIST_ENTITIES_SUCCESS,
+  LIST_MORE_ENTITIES
 } from '@app/store/entities/operations/list/constants';
 import { ListEntitiesActionTypes } from '@app/store/entities/operations/list/models';
 
 export function listEntitiesStart(): ListEntitiesActionTypes {
   return {
     type: LIST_ENTITIES_START
+  };
+}
+
+export function listMoreEntities(): ListEntitiesActionTypes {
+  return {
+    type: LIST_MORE_ENTITIES
   };
 }
 
--- src/store/entities/reducer.ts
+++ src/store/entities/reducer.ts
@@ -5,7 +5,10 @@ import { CREATE_ENTITY_SUCCESS } from '@app/store/entities/operations/create/con
 import createReducer from '@app/store/entities/operations/create/reducer';
 import { DELETE_ENTITY_SUCCESS } from '@app/store/entities/operations/delete/constants';
 import deleteReducer from '@app/store/entities/operations/delete/reducer';
-import { LIST_ENTITIES_SUCCESS } from '@app/store/entities/operations/list/constants';
+import {
+  LIST_ENTITIES_START,
+  LIST_ENTITIES_SUCCESS
+} from '@app/store/entities/operations/list/constants';
 import listReducer from '@app/store/entities/operations/list/reducer';
 import readReducer from '@app/store/entities/operations/read/reducer';
 import { UPDATE_ENTITY_SUCCESS } from '@app/store/entities/operations/update/constants';
@@ -13,9 +16,17 @@ import updateReducer from '@app/store/entities/operations/update/reducer';
 
 const reducer: Reducer<IEntity[], EntityActionTypes> = (state = [], action) => {
   switch (action.type) {
+    case LIST_ENTITIES_START: {
+      return [];
+    }
     case LIST_ENTITIES_SUCCESS: {
       const { entities } = action.payload;
-      return entities;
+      return [...state, ...entities].reduce((unique, entity) => {
+        if (!unique.find(({ objectId }) => objectId === entity.objectId)) {
+          unique.push(entity);
+        }
+        return unique;
+      }, []);
     }
     case CREATE_ENTITY_SUCCESS: {
       const { entity } = action.payload;
@@ -23,12 +34,12 @@ const reducer: Reducer<IEntity[], EntityActionTypes> = (state = [], action) => {
     }
     case UPDATE_ENTITY_SUCCESS: {
       const { entity } = action.payload;
-      const idx = state.map((item) => item.objectId).indexOf(entity.objectId);
+      const idx = state.findIndex((item) => item.objectId === entity.objectId);
       return [...state.slice(0, idx), entity, ...state.slice(idx + 1)];
     }
     case DELETE_ENTITY_SUCCESS: {
       const { id } = action.payload;
-      const idx = state.map((item) => item.objectId).indexOf(id);
+      const idx = state.findIndex((item) => item.objectId === id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
     }
     default: {
