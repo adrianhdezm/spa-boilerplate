4d1419dd9002c3ab9a19c02fb2ed58e5bda4a4d8
Adrian Hernandez-Mendez <aahdezm@gmail.com>
2019-06-15 21:16:26 +0200

    Add api component based on Graphql

---
+++ src/services/api/graphql/mutations.ts
@@ -0,0 +1,27 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const createEntity = `mutation CreateEntity($input: CreateEntityInput!) {
+  createEntity(input: $input) {
+    id
+    name
+    description
+  }
+}
+`;
+export const updateEntity = `mutation UpdateEntity($input: UpdateEntityInput!) {
+  updateEntity(input: $input) {
+    id
+    name
+    description
+  }
+}
+`;
+export const deleteEntity = `mutation DeleteEntity($input: DeleteEntityInput!) {
+  deleteEntity(input: $input) {
+    id
+    name
+    description
+  }
+}
+`;
---
+++ src/services/api/graphql/queries.ts
@@ -0,0 +1,26 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const getEntity = `query GetEntity($id: ID!) {
+  getEntity(id: $id) {
+    id
+    name
+    description
+  }
+}
+`;
+export const listEntitys = `query ListEntitys(
+  $filter: ModelEntityFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listEntitys(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      name
+      description
+    }
+    nextToken
+  }
+}
+`;
