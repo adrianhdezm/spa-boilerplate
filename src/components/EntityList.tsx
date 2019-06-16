a388c3e041bdbbfbbac09496a2c9742d6abc774c
Adrian Hernandez-Mendez <aahdezm@gmail.com>
2019-06-16 20:40:02 +0200

    Add Entity Delete feature

--- src/components/EntityList.tsx
+++ src/components/EntityList.tsx
@@ -3,13 +3,28 @@ import { Link } from 'react-router-dom';
 
 import { IEntity } from '@app/models';
 
-const EntityList: React.FC<{ data: IEntity[] }> = ({ data }) => {
-  const entities = data.map((item: IEntity, index: number) => (
-    <div key={index}>
-      <Link to={`/${item.id}`}>{item.name}</Link>
-      <Link to={`/${item.id}/edit`}>Edit</Link>
-    </div>
-  ));
+const EntityList: React.FC<{ data: IEntity[]; onDelete: (id: string) => void }> = ({
+  data,
+  onDelete
+}) => {
+  const handleDelete = (id: string) => {
+    if (id) {
+      onDelete(id);
+    }
+  };
+
+  const entities = data.map((item: IEntity, index: number) => {
+    const handleClick = () => handleDelete(item.id);
+    return (
+      <div key={index}>
+        <Link to={`/${item.id}`}>{item.name}</Link>
+        <Link to={`/${item.id}/edit`}>Edit</Link>
+        <button type="button" onClick={handleClick}>
+          Delete
+        </button>
+      </div>
+    );
+  });
   return <>{entities}</>;
 };
 
--- src/views/EntityListView.tsx
+++ src/views/EntityListView.tsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
 import EntityList from '@app/components/EntityList';
 import PageLayout from '@app/components/PageLayout';
 import { IEntity } from '@app/models';
+import { deleteEntity } from '@app/services/api/graphql/mutations';
 import { listEntitys } from '@app/services/api/graphql/queries';
-import { ListEntitysQuery } from '@app/services/api/models';
+import { DeleteEntityMutation, ListEntitysQuery } from '@app/services/api/models';
 import { useGqlQuery } from '@app/utils/hooks';
+import API, { graphqlOperation } from '@aws-amplify/api';
+import { GraphQLResult } from '@aws-amplify/api/lib/types';
 
 const EntityListView: React.FC<RouteComponentProps<{}>> = () => {
+  const [entities, setEntities] = useState<IEntity[]>([]);
+
   const { isLoading, data } = useGqlQuery<ListEntitysQuery>(listEntitys);
 
-  const entities = data && data.listEntitys && data.listEntitys.items ? data.listEntitys.items : [];
+  const handleDelete = async (id: string) => {
+    try {
+      const { query, variables } = graphqlOperation(deleteEntity, { input: { id } });
+      const hasValidMutation = query && API.getGraphqlOperationType(query) === 'mutation';
+      if (hasValidMutation) {
+        const response = await API.graphql({ query, variables });
+        const { errors, data: responceData } = response as GraphQLResult;
+        const responceEntity = (responceData as DeleteEntityMutation).deleteEntity;
+        const idx = entities.findIndex((item) => item.id === (responceEntity as IEntity).id);
+        setEntities([...entities.slice(0, idx), ...entities.slice(idx + 1)]);
+
+        if (errors && errors.length > 0) {
+          throw new Error('Response Error');
+        }
+      }
+    } catch (error) {
+      throw new Error('Delete Error');
+    }
+  };
+
+  useEffect(() => {
+    if (data && data.listEntitys && data.listEntitys.items) {
+      const items = data.listEntitys.items;
+      setEntities(items as IEntity[]);
+    }
+  }, [data]);
+
   return (
     <PageLayout loading={isLoading || entities.length === 0}>
-      <EntityList data={entities as IEntity[]} />
+      <EntityList data={entities} onDelete={handleDelete} />
     </PageLayout>
   );
 };
